f2cce68fabdcb2c4435c51f192cf1232d2392a91
JORGE_ARTURO_MEZA_MENDEZ <a.21308051280504@cbtis128.edu.mx>
2024-02-26 18:03:17 +0000

    actualizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,101 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MiDrawerMeza());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiDrawerMeza extends StatelessWidget {
+  const MiDrawerMeza({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: true,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Drawer Meza"),
+          centerTitle: true,
+          backgroundColor: Colors.deepOrange,
+          actions: [
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.more_vert,
+              ),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.more_sharp,
+              ),
+              onPressed: () {},
+            )
+          ],
+        ),
+        backgroundColor: const Color(0xffdbdbdb),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              UserAccountsDrawerHeader(
+                accountName: Text("Refaccionaria"),
+                accountEmail: Text("a.21308051280504@cbtis128.edu.mx"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://raw.githubusercontent.com/JorgeMeza123/practica1_6j_IOS/main/draw1.jpg"),
+                ),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      "https://raw.githubusercontent.com/JorgeMeza123/practica1_6j_IOS/main/draw5.png",
+                    ),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                otherAccountsPictures: [
+                  CircleAvatar(
+                    backgroundColor: Colors.white,
+                    backgroundImage: NetworkImage(
+                        "https://raw.githubusercontent.com/JorgeMeza123/practica1_6j_IOS/main/draw2.jpg"),
+                  ),
+                  CircleAvatar(
+                    backgroundColor: Colors.white,
+                    backgroundImage: NetworkImage(
+                        "https://raw.githubusercontent.com/JorgeMeza123/practica1_6j_IOS/main/draw3.jpg"),
+                  ),
+                ],
+              ),
+              const ListTile(
+                leading:
+                    Icon(Icons.account_box_sharp, color: Colors.orangeAccent),
+                title: Text("Usuario",
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+              ),
+              const ListTile(
+                leading:
+                    Icon(Icons.account_box_rounded, color: Color(0xffffd340)),
+                title: Text("Empleado",
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+              ),
+              const ListTile(
+                leading:
+                    Icon(Icons.list_alt_outlined, color: Color(0xffd0a925)),
+                title: Text("Clientes",
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+              ),
+              const ListTile(
+                leading: Icon(Icons.list_alt_rounded, color: Color(0xff977301)),
+                title: Text("inventario",
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+              ),
+              const ListTile(
+                leading: Icon(Icons.abc_outlined, color: Color(0xff977301)),
+                title: Text("ventas",
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+              )
+            ],
+          ),
         ),
       ),
     );
